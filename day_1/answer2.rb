eb154e38a47e4d3c5a8994485addd907fab27a5c
Jerry Chen <jerry.chen@aktsk.com>
2021-12-03 17:20:27 +0800

    day 1

---
+++ day_1/answer2.rb
@@ -0,0 +1,8 @@
+# https://adventofcode.com/2021/day/1
+
+dir = File.dirname(__FILE__)
+
+count = File.read("#{dir}/input").lines.map(&:to_i).each_cons(3).map(&:sum).each_cons(2).count { |(previous, current)|
+  current > previous
+}
+puts count
